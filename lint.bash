8dbfb9f08dc0abc6401640a4ebdac7f433b0bc59
HARUYAMA Seigo <haruyama@unixuser.org>
2020-11-04 21:25:12 +0900

    shellcheck を追加

--- lint.bash
+++ lint.bash
@@ -10,3 +10,4 @@ cppcheck --language=c++ "${targets[@]}" || exit 1
 cpplint --linelength=120 --filter=-build/include_subdir,-legal/copyright,-build/c++11 "${targets[@]}" || exit 1
 # clang-tidy -p debug/compile_commands.json "${targets[@]}"
 misspell -error "${targets[@]}"
+shellcheck ./**/*.bash
