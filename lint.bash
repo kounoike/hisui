d2b8f973722a70ddfe2408db30c748e84300dfaa
HARUYAMA Seigo <haruyama@unixuser.org>
2020-11-04 21:33:28 +0900

    exit 1 を追加

--- lint.bash
+++ lint.bash
@@ -8,6 +8,5 @@ targets=(src/**/*.[ch]pp test/**/*.[ch]pp)
 
 cppcheck --language=c++ "${targets[@]}" || exit 1
 cpplint --linelength=120 --filter=-build/include_subdir,-legal/copyright,-build/c++11 "${targets[@]}" || exit 1
-# clang-tidy -p debug/compile_commands.json "${targets[@]}"
-misspell -error "${targets[@]}"
-shellcheck ./**/*.bash
+misspell -error "${targets[@]}" || exit 1
+shellcheck ./**/*.bash || exit 1
