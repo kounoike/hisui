67ca437ec08de2f013524ab528d3ce292b8d08e1
HARUYAMA Seigo <haruyama@tankyu.net>
2021-04-16 15:40:23 +0900

    コマンドラインオプションの調整

--- src/config.cpp
+++ src/config.cpp
@@ -17,6 +17,8 @@
 #include <CLI/Validators.hpp>
 #include <boost/rational.hpp>
 
+#define EXPERIMENTAL_OPTIONS "Experimental Options"
+
 #ifdef NDEBUG
 #define OPTIONS_FOR_TUNING ""
 #define OPTIONS_FOR_DEVELOPING ""
@@ -64,28 +66,33 @@ void set_cli_options(CLI::App* app, Config* config) {
 
   app->add_option("--screen-capture-report",
                   config->screen_capture_metadata_filename,
-                  "Multi Channel Metadata filename")
-      ->check(CLI::ExistingFile);
+                  "Screen Capture Metadata filename")
+      ->check(CLI::ExistingFile)
+      ->group(EXPERIMENTAL_OPTIONS);
 
   app->add_option("--screen-capture-width", config->screen_capture_width,
-                  "Width for screen-capture (960)");
+                  "Width for screen-capture. default: 960")
+      ->group(EXPERIMENTAL_OPTIONS);
 
   app->add_option("--screen-capture-height", config->screen_capture_height,
-                  "Height for screen-capture (640)");
+                  "Height for screen-capture. default: 640")
+      ->group(EXPERIMENTAL_OPTIONS);
 
   app->add_option("--screen-capture-bit-rate", config->screen_capture_bit_rate,
-                  "Bit rate for screen-capture (1000)");
+                  "Bit rate for screen-capture (kbps). default: 1000")
+      ->group(EXPERIMENTAL_OPTIONS);
 
   app->add_option("--mix-screen-capture-audio",
                   config->mix_screen_capture_audio,
-                  "Mix screen-capture audio (false)");
+                  "Mix screen-capture audio. default: false)")
+      ->group(EXPERIMENTAL_OPTIONS);
 
   std::vector<std::pair<std::string, config::OutContainer>> out_container_assoc{
       {"WebM", config::OutContainer::WebM},
       {"MP4", config::OutContainer::MP4},
   };
   app->add_option("--out-container", config->out_container,
-                  "Output container type (WebM/MP4) default: WebM")
+                  "Output container type (WebM/MP4). default: WebM")
       ->transform(
           CLI::CheckedTransformer(out_container_assoc, CLI::ignore_case));
 
@@ -94,7 +101,7 @@ void set_cli_options(CLI::App* app, Config* config) {
       {"VP9", config::OutVideoCodec::VP9},
   };
   app->add_option("--out-video-codec", config->out_video_codec,
-                  "Video codec (VP8/VP9) default: VP9")
+                  "Video codec (VP8/VP9). default: VP9")
       ->transform(CLI::CheckedTransformer(out_video_codec_assoc));
 
   std::vector<std::pair<std::string, config::OutAudioCodec>> out_audio_codec{
@@ -105,7 +112,7 @@ void set_cli_options(CLI::App* app, Config* config) {
 #endif
   };
   app->add_option("--out-audio-codec", config->out_audio_codec,
-                  "Audio codec (Opus/AAC) default: Opus (hisui supports "
+                  "Audio codec (Opus/AAC). default: Opus (hisui supports "
                   "AAC only in WebM)")
 #ifndef USE_FDK_AAC
       ->group("")
@@ -113,27 +120,27 @@ void set_cli_options(CLI::App* app, Config* config) {
       ->transform(CLI::CheckedTransformer(out_audio_codec, CLI::ignore_case));
 
   app->add_option("--out-video-frame-rate", config->out_video_frame_rate,
-                  "Video frame rate (INTEGER/RATIONAL) default: 25)");
+                  "Video frame rate (INTEGER/RATIONAL). default: 25)");
 
   app->add_option("--out-webm-file", config->out_filename, "Output filename")
       ->group(OPTIONS_FOR_BACKWARD_COMPATIBILITY);
   app->add_option("--out-file", config->out_filename, "Output filename");
 
   app->add_option("--max-columns", config->max_columns,
-                  "Max columns (POSITIVE INTEGER) default: 3")
+                  "Max columns (POSITIVE INTEGER). default: 3")
       ->check(CLI::PositiveNumber);
 
   app->add_option(
          "--libvpx-cq-level", config->libvpx_cq_level,
-         "libvpx Constrained Quality level (NON NAGATIVE INTEGER) default: 10")
+         "libvpx Constrained Quality level (NON NAGATIVE INTEGER). default: 30")
       ->check(CLI::Range(0, 63));
   app->add_option(
          "--libvpx-min-q", config->libvpx_min_q,
-         "libvpx minimum (best) quantizer (NON NEGATIVE INTEGER) default: 3")
+         "libvpx minimum (best) quantizer (NON NEGATIVE INTEGER). default: 10")
       ->check(CLI::Range(0, 63));
   app->add_option(
          "--libvpx-max-q", config->libvpx_max_q,
-         "libvpx maximum (worst) quantizer (NON NEGATIVE INTEGER) default: 40")
+         "libvpx maximum (worst) quantizer (NON NEGATIVE INTEGER). default: 50")
       ->check(CLI::Range(0, 63));
 
   app->add_option("--out-opus-bit-rate", config->out_opus_bit_rate,
@@ -176,13 +183,13 @@ void set_cli_options(CLI::App* app, Config* config) {
   app->add_option(
          "--scaling-width", config->scaling_width,
          "Scaling width per grid (NON NAGATIVE multiple of 4. auto mode: "
-         "0) default: 320")
+         "0). default: 320")
       ->check(NonNegativeMultipleOf4)
       ->group(OPTIONS_FOR_TUNING);
 
   app->add_option("--scaling-height", config->scaling_height,
                   "Scaling height per grid (NON NAGATIVE multiple of 4, auto "
-                  "mode: 0) default: 240")
+                  "mode: 0). default: 240")
       ->check(NonNegativeMultipleOf4)
       ->group(OPTIONS_FOR_TUNING);
 
@@ -194,37 +201,37 @@ void set_cli_options(CLI::App* app, Config* config) {
           {"box", libyuv::kFilterBox},
       };
   app->add_option("--libyuv-filter-mode", config->libyuv_filter_mode,
-                  "libyuv filter mode (none/linear/bilinear/box) default: box")
+                  "libyuv filter mode (none/linear/bilinear/box). default: box")
       ->transform(
           CLI::CheckedTransformer(libyuv_filter_mode_assoc, CLI::ignore_case))
       ->group(OPTIONS_FOR_TUNING);
 
   app->add_option("--libvpx-threads", config->libvpx_threads,
-                  "libvpx max number of threads (NON NEGATIVE INTEGER) "
+                  "libvpx max number of threads (NON NEGATIVE INTEGER). "
                   "default: 0 (use default)")
       ->check(CLI::NonNegativeNumber)
       ->group(OPTIONS_FOR_TUNING);
 
   app->add_option("--libvp9-frame-parallel", config->libvp9_frame_parallel,
-                  "libvp9 frame parallel decodability features "
+                  "libvp9 frame parallel decodability features. "
                   "default: 1")
       ->check(CLI::Range(0, 1))
       ->group(OPTIONS_FOR_TUNING);
 
   app->add_option("--libvpx-cpu-used", config->libvpx_cpu_used,
-                  "libvpx cpu used (-16, 16) "
+                  "libvpx cpu used (-16, 16). "
                   "default: 4")
       ->check(CLI::Range(-16, 16))
       ->group(OPTIONS_FOR_TUNING);
 
   app->add_option("--libvp9-tile-columns", config->libvp9_tile_columns,
-                  "libvp9 number of tile columns to use, log2 "
+                  "libvp9 number of tile columns to use, log2. "
                   "default: 0 (0, 6)")
       ->check(CLI::Range(0, 6))
       ->group(OPTIONS_FOR_TUNING);
 
   app->add_option("--libvp9-row-mt", config->libvp9_row_mt,
-                  "libvp9 row based non-deterministic multi-threading"
+                  "libvp9 row based non-deterministic multi-threading. "
                   "default: 0 (0, 1)")
       ->check(CLI::Range(0, 1))
       ->group(OPTIONS_FOR_TUNING);
@@ -241,7 +248,7 @@ void set_cli_options(CLI::App* app, Config* config) {
       };
   app->add_option(
          "-l,--log-level", config->log_level,
-         "Log level (trace/debug/info/warn/error/critical/off) default: info")
+         "Log level (trace/debug/info/warn/error/critical/off). default: info")
       ->transform(CLI::CheckedTransformer(log_level_assoc, CLI::ignore_case))
       ->group(OPTIONS_FOR_DEVELOPING);
 
