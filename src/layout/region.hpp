b95db9d512e1b43bff1c348b10fe28a2cd2c99bf
HARUYAMA Seigo <haruyama@tankyu.net>
2021-10-12 16:02:34 +0900

    Add hisui::layout::{Cell,calc_cell_length_and_positions()}

--- src/layout/region.hpp
+++ src/layout/region.hpp
@@ -5,21 +5,12 @@
 #include <string>
 #include <vector>
 
+#include "layout/cell.hpp"
 #include "layout/grid.hpp"
 #include "layout/source.hpp"
 #include "video/source.hpp"
 
 namespace hisui::layout {
-
-struct Position {
-  std::uint64_t x = 0;
-  std::uint64_t y = 0;
-};
-
-struct Resolution {
-  std::uint64_t width = 0;
-  std::uint64_t height = 0;
-};
 
 enum Reuse {
   None,
@@ -54,6 +45,7 @@ class Region {
   // computed
   GridDimension m_grid_dimension;
   std::vector<std::shared_ptr<VideoSource>> m_video_sources;
+  std::vector<std::shared_ptr<Cell>> m_cells;
   std::uint64_t m_max_end_time;
 };
 
